b274c85b37f7d0f8213bc0881bc6a5d223a36a8e
MesVortex <meskinemostafa4@gmail.com>
2024-10-27 20:11:15 +0100

    WOR-61 Configure SLF4J logger

--- worlds24_structure/src/main/java/org/esports/Main.java
+++ worlds24_structure/src/main/java/org/esports/Main.java
@@ -8,7 +8,6 @@ public class Main {
     public static void main(String[] args) {
         ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
         ConsoleUI consoleUI = (ConsoleUI) context.getBean("consoleUI");
-
         consoleUI.showMenu();
     }
 }
